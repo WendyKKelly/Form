3b0a54ba7cdb4cdd30520a1b8613cd3c5cbf5b98
Wendy Kelly <wendykelly@Wendys-iMac.local>
2023-02-25 09:55:00 -0800

    add env var - without next public

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
+            <code className={styles.code}>Hello</code>
           </p>
           <div>
             <a
